75bd7851581d7c51ae8e60cd05d921b005e439b3
lukasini <lukasvarhol@yahoo.com>
2020-11-17 01:51:53 +0100

    Update hashing.py

--- hashing.py
+++ hashing.py
@@ -4,7 +4,7 @@ def encrypt_string(hash_string):
     sha_signature = \
         hashlib.sha256(hash_string.encode()).hexdigest()
     return sha_signature
-sex = input("Insert text to be converted into hash:")
-hash_string = sex
+x = input("Insert text to be converted into hash:")
+hash_string = x
 sha_signature = encrypt_string(hash_string)
 print(sha_signature)
